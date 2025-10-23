d88bb13125f5607d75c32a8546676f5801c3a3da
Adrielly Souza <adriellykerolyn@gmail.com>
2025-10-23 13:33:55 -0300

    Adiciona exercício 5 para teste de branch

---
+++ exercicio5.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que peça ao usuário para inserir as notas de três provas e calcule a média aritmética dessas notas.
+
+p1 = float(input("Insira a nota 1:"))
+p2 = float(input("Insira a nota 2:"))
+p3 = float(input("Insira a nota 3:"))
+
+calc = (p1 + p2 + p3) / 3
+
+print("A sua média foi:", calc)
